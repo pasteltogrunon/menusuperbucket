9491082ea6caf8c300e2507e3669a30d275d96ff
EPG <juanfernandezesteban44@gmail.com>
2024-05-18 13:35:08 +0200

    Damage

--- menusuperbucket/Assets/Script/DimensionSwap.cs
+++ menusuperbucket/Assets/Script/DimensionSwap.cs
@@ -12,13 +12,11 @@ public class DimensionSwap : MonoBehaviour
     [SerializeField] GameObject Astralis;
 
 
-    // Start is called before the first frame update
     void Awake()
     {
         Instance = this;
     }
 
-    // Update is called once per frame
     void Update()
     {
 
---
+++ menusuperbucket/Assets/Script/PlayerHurt.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHurt : MonoBehaviour
+{
+    [SerializeField] float invulnerableTime = 0.5f;
+    [SerializeField] Vector2 damageKnockback;
+    [SerializeField] LayerMask enemyLayer;
+
+    bool isInvulnerable = false;
+
+    BoxCollider2D hitbox;
+
+    void Awake()
+    {
+        hitbox = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        //Only compute if not invulnerable
+        if (isInvulnerable) return;
+
+        Collider2D[] hit = Physics2D.OverlapBoxAll(new Vector2(transform.position.x, transform.position.y) + hitbox.offset, hitbox.size, 0, enemyLayer);
+
+        if(hit.Length != 0)
+        {
+            //Health damage
+            GetComponent<HealthManager>().Health -= 5;
+
+            //Damage knockback
+            float enemyDirection = Mathf.Sign(transform.position.x - hit[0].transform.position.x);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(damageKnockback.x * enemyDirection, damageKnockback.y + GetComponent<Rigidbody2D>().velocity.y);
+            
+            //Invulnerability
+            StartCoroutine(damageCooldown());
+        }
+
+    }
+
+    IEnumerator damageCooldown()
+    {
+        isInvulnerable = true;
+        yield return new WaitForSeconds(invulnerableTime);
+        isInvulnerable = false;
+    }
+}
