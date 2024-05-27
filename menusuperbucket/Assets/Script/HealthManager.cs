4d50744a7e1a4670455712efc1dafd5dc60b8c3e
EPG <juanfernandezesteban@gmail.com>
2024-05-27 19:50:40 +0200

    Some VFX

--- menusuperbucket/Assets/Script/HealthManager.cs
+++ menusuperbucket/Assets/Script/HealthManager.cs
@@ -7,11 +7,18 @@ public class HealthManager : MonoBehaviour
     [SerializeField] int maxHealth = 10;
     [SerializeField] int health;
 
+    [SerializeField] GameObject deathParticles;
+
     public int Health
     {
         get => health;
         set
         {
+            if(value < health)
+            {
+                StartCoroutine(damageFX());
+            }
+
             health = Mathf.Clamp(value, 0, maxHealth);
             if(health == 0)
             {
@@ -28,6 +35,24 @@ public class HealthManager : MonoBehaviour
     
     void die()
     {
+        if(deathParticles != null)
+        {
+            Instantiate(deathParticles, transform.position, Quaternion.identity);
+        }
         Destroy(gameObject);
+    }
+
+    IEnumerator damageFX()
+    {
+        Material material = GetComponent<Renderer>().material;
+        if(material.HasFloat("_Damage"))
+        {
+            for(float t = 0; t<= 0.2; t+= Time.deltaTime)
+            {
+                material.SetFloat("_Damage", 1 - t / 0.2f);
+                yield return null;
+            }
+            material.SetFloat("_Damage", 0);
+        }
     }
 }
